8a68beeb30e213cc2d88b1b1707bc78a06e858af
Krishnakumar <krish198.ky@gmail.com>
2021-07-14 21:32:17 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.fonts1').click(function() {
+    $('.fonts').slideToggle("slow");
+    // Alternative animation for example
+    // slideToggle("fast");
+  });
+});
+});
